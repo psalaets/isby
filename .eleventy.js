89f7fd286b99b05b65cba5f32b20951f810783b9
Paul Salaets <psalaets@gmail.com>
2019-09-18 17:10:48 -0400

    post process css a bit

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,23 @@
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+
+// replace this with cssnano/postcss after eleventy adds supports for async
+// shortcodes (maybe in 0.9.1)
+const CleanCss = require('clean-css');
+
+const postCssProcessor = postcss([
+  autoprefixer
+]);
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter('tail', function(array, count) {
     return array.slice(-count);
+  });
+
+  eleventyConfig.addPairedShortcode('style', function(rawCss) {
+    const prefixed = postCssProcessor.process(rawCss, {from: undefined}).css;
+    const minified = new CleanCss({}).minify(prefixed).styles;
+
+    return `<style>${minified}</style>`;
   });
 };
