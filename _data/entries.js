a4a21a9c5dc2b8a1b89841554d0e28eb9f74ec6d
Paul Salaets <psalaets@gmail.com>
2021-10-22 19:41:54 -0400

    Point at image's page

--- _data/entries.js
+++ _data/entries.js
@@ -1,6 +1,12 @@
 module.exports = [
   {
     answer: 'Soon',
+    url: 'https://imagecomics.com/comics/releases/saga-55',
+    blurb: null,
+    date: '2021-10-22'
+  },
+  {
+    answer: 'Soon',
     url: 'https://www.instagram.com/p/CU0X4PAvtDJ/',
     blurb: null,
     date: '2021-10-09'
