461b11813dcee48fe695e0dd6c3efe3ebf28e48a
Paul Salaets <psalaets@gmail.com>
2023-07-25 07:30:10 -0700

    Update date

--- _data/entries.js
+++ _data/entries.js
@@ -2,6 +2,12 @@ module.exports = [
   {
     answer: 'Soon',
     url: 'https://imagecomics.com/comics/releases/saga-66-mr',
+    blurb: 'Chapter 66 on August 30, 2023',
+    date: '2023-07-25'
+  },
+  {
+    answer: 'Soon',
+    url: 'https://imagecomics.com/comics/releases/saga-66-mr',
     blurb: 'Chapter 66 on August 23, 2023',
     date: '2023-07-10'
   },
